d64984fed56980f5dbc812513e0f9d28380d7ec9
null-route-dev <null-route-dev@protonmail.com>
2026-06-25 23:48:35 +0300

    feat: add user schemas

---
+++ app/schemas/user_schemas.py
@@ -0,0 +1,16 @@
+from pydantic import BaseModel
+
+class CreateUser(BaseModel):
+    username: str
+    email: str
+    password: str
+
+class LoginUser(BaseModel):
+    email: str
+    password: str
+
+class UserResponse(BaseModel):
+    username: str
+    email: str
+    created_at: str
+    updated_at: str
